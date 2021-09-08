5d02e0482f55b0bba17e1bac334a520243b1019c
yizhang0301 <43011146+yizhang0301@users.noreply.github.com>
2021-09-08 17:40:44 +0800

    Update P161.cpp

--- Assignments/P161/P161.cpp
+++ Assignments/P161/P161.cpp
@@ -8,30 +8,13 @@
 #include <iomanip>
 using namespace std;
 
-vector<int> v;
-int minCycle;
-
-int get_res()
-{
-	int time = minCycle * 2;
-	int sz = v.size();
-	int cnt = 0;
-
-	while (cnt < sz && time <= 18000) {
-		for (cnt = 0; cnt < sz; ++cnt) {
-			if (!(time % (v[cnt] * 2) < v[cnt] - 5)) break;
-		}
-		++time;
-	}
-
-	if (--time <= 18000 && cnt == sz) return time;
-	else return -1;
-}
+vector<int> v;   //vector for
+int minCycle;   //create cycle 
 
 int main()
 {
 	int num1, num2, num3;
-
+	 //  read each 3 data for a group
 	while (cin >> num1 >> num2 >> num3, num1 || num2 || num3) {
 		v.clear();
 
@@ -42,6 +25,7 @@ int main()
 		if (num2 < minCycle) minCycle = num2;
 
 		if (num3) {
+			//checking next int at end of loop
 			v.push_back(num3);
 			if (num3 < minCycle) minCycle = num3;
 
@@ -52,8 +36,9 @@ int main()
 		}
 
 		int ans = get_res();
-
+		//print 5 hour failure message
 		if (ans == -1) cout << "Signals fail to synchronise in 5 hours\n";
+		//get time and print out timeframe
 		else {
 			int h, m;
 			h = ans / 3600;
@@ -69,3 +54,21 @@ int main()
 
 	return 0;
 }
+
+int get_res()
+{
+	int time = minCycle * 2;
+	int sz = v.size();
+	int cnt = 0;
+	//find answer in 1-18000 s 
+	while (cnt < sz && time <= 18000) {
+		for (cnt = 0; cnt < sz; ++cnt) {
+			if (!(time % (v[cnt] * 2) < v[cnt] - 5)) break;
+		}
+		++time;
+	}
+
+	if (--time <= 18000 && cnt == sz) return time;
+	else return -1;
+}
+
